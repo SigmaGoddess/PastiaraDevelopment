6b5c75eebe0ab5b0988f854280e37fb3c6bd05f7
Victor Manuel Martinez <vicmanuelg19@gmail.com>
2025-09-19 18:51:46 +0000

    feet:se agregó dinamismo a los links y se agrega collapse para mostrar form

--- js/administrador.js
+++ js/administrador.js
@@ -1,5 +1,6 @@
+ /*
  // Obtener todos los links
-  const navLinks = document.querySelectorAll('#menu .nav-link');
+  const navLinks = document.querySelectorAll('#menu .nav-link, #menu #list-link');
 // para cada link aplicamos la función 
   navLinks.forEach(link => {
     link.addEventListener('click', function() {
@@ -9,5 +10,28 @@
       this.classList.add('active');
     });
   });
+*/
 
+ // Seleccionamos todos los links del navbar y del menú lateral
+    const allLinks = document.querySelectorAll('#menu .nav-link, #menu #list-link');
+    console.log(allLinks);
+
+    allLinks.forEach(link => {
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+
+        // Obtener el identificador (ej: "inicio", "servicios", etc.)
+        const name = this.dataset.name;
+        console.log(name);
+        // Quitar 'active' de todos los links
+        allLinks.forEach(l => l.classList.remove('active'));
+
+        // Agregar 'active' a todos los links con el mismo data-name
+        document.querySelectorAll(`[data-name="${name}"]`).forEach(l => {
+          
+          l.classList.add('active');
+        });
+      });
+    });
   
