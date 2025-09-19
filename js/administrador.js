377da8b8543531395ae0c87bb135a71b10d3dd31
Victor Martinez <vicmanuelg19@gmail.com>
2025-09-19 17:55:21 -0600

    feet: Se agregó dinamismo en como se despliegan los form de cada opción de administrador

--- js/administrador.js
+++ js/administrador.js
@@ -1,20 +1,8 @@
- /*
- // Obtener todos los links
-  const navLinks = document.querySelectorAll('#menu .nav-link, #menu #list-link');
-// para cada link aplicamos la función 
-  navLinks.forEach(link => {
-    link.addEventListener('click', function() {
-      // Quitar "active" de todos
-      navLinks.forEach(l => l.classList.remove('active'));
-      // Agregar "active" solo al que se le dió click
-      this.classList.add('active');
-    });
-  });
-*/
 
  // Seleccionamos todos los links del navbar y del menú lateral
     const allLinks = document.querySelectorAll('#menu .nav-link, #menu #list-link');
-    console.log(allLinks);
+
+    
 
     allLinks.forEach(link => {
       link.addEventListener('click', function (e) {
@@ -23,9 +11,10 @@
 
         // Obtener el identificador (ej: "inicio", "servicios", etc.)
         const name = this.dataset.name;
-        console.log(name);
         // Quitar 'active' de todos los links
-        allLinks.forEach(l => l.classList.remove('active'));
+        allLinks.forEach(l =>{
+           l.classList.remove('active')
+          });
 
         // Agregar 'active' a todos los links con el mismo data-name
         document.querySelectorAll(`[data-name="${name}"]`).forEach(l => {
@@ -34,4 +23,8 @@
         });
       });
     });
+
+
+
+//
   
