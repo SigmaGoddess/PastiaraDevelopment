4da125ec0c73ad37ab8a0f83cf2a3e0d92ff0607
Jessica Martinez <jessica.martinezalv2905@gmail.com>
2025-09-24 02:40:39 -0600

    feat: Se agrega por primera vez los documentos HTML, CSS Y JS para la sección conocenos

---
+++ js/conocenos.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Se coloco la clase "fade-in"
+    const elementosAnimados = document.querySelectorAll('.fade-in');
+
+    // Crea un nuevo observador
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            // Si el elemento es visible
+            if (entry.isIntersecting) {
+                // Se coloca la clase 'is-visible' para activar la animación
+                entry.target.classList.add('is-visible');
+                // Deja de observar el elemento una vez que la animación se activa
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        // La animación se activa cuando el 10% del elemento sea visible
+        threshold: 0.1
+    });
+
+    // Observa cada uno de los elementos seleccionados
+    elementosAnimados.forEach(elemento => {
+        observer.observe(elemento);
+    });
+});
