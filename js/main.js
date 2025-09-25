c39f77a8841f9923dbc5016515ea30567b8d9e1a
Sheila Miranda <shei_xdxdxd@hotmail.com>
2025-09-25 00:39:29 -0600

    feat: integración de la homepage al index y eliminación de la misma, cambios en el hompage css

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-import "./css/normalize.css"; // CSS reset
+import "../css/normalize.css"; // CSS reset
 
 // Importing CSS files
-import "./css/administador.css"; // Admin styles
-import "./css/navbar.css"; // Navbar styles
-import "./css/registro.css"; // Registro styles
+import "../css/administador.css"; // Admin styles
+import "../css/navbar.css"; // Navbar styles
+import "../css/registro.css"; // Registro styles
+import "../css/homePage.css"; // Home page styles
 
 // Global variables
-import "./css/global_variables.css"; // Global CSS variables
+import "../css/global_variables.css"; // Global CSS variables
 
 //Components
 import "./components.js"; // Component loader
