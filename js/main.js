5f48f7bcc62b0876dbbc5f0b9d037234c5750ae5
Sheila Miranda <shei_xdxdxd@hotmail.com>
2025-09-26 16:58:44 -0600

    feat: se agregó vite al proyecto y cambios generales en el main js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 import "../css/normalize.css"; // CSS reset
 
-// Importing CSS files
-import "../css/administador.css"; // Admin styles
-import "../css/navbar.css"; // Navbar styles
-import "../css/registro.css"; // Registro styles
-import "../css/homePage.css"; // Home page styles
-
 // Global variables
 import "../css/global_variables.css"; // Global CSS variables
 
