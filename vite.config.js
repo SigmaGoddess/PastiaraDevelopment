5219252a678ade06c26daf08a3ae8e6b77f9b993
Melissa Flores <melissaf.garza@gmail.com>
2025-10-03 14:02:05 -0600

    feat: se inyecta navbar con tag special header

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 conocenos: resolve(__dirname, 'pages/pag-conocenos/conocenos.html'),
-                menu: resolve(__dirname, 'pages/pag-navbar/navbar.html')
+                menu: resolve(__dirname, 'pages/pag-navbar/navbar.html'),
                 /*aboutUs: resolve(__dirname, 'src/pages/about-us/about-us.html'),
-                contact: resolve(__dirname, 'src/pages/contact/contact.html'),
-                register: resolve(__dirname, 'src/pages/register/register.html'),*/
+                contact: resolve(__dirname, 'src/pages/contact/contact.html'),*/
+                registro: resolve(__dirname, 'pages/pag-registro/registro.html'),
             },
         },
     },
