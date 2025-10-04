912db949dbc4f9e8770f7ddcdc63795801975aab
Sheila Miranda <shei_xdxdxd@hotmail.com>
2025-10-03 19:37:32 -0600

    feat: cambios en el swiper del home page y se anadio parte de las preguntas frecuentes

--- js/components.js
+++ js/components.js
@@ -17,3 +17,22 @@ class SpecialHeader extends HTMLElement {
 }
 
 customElements.define('special-header', SpecialHeader);
+
+/**class SpecialFooter extends HTMLElement {
+    async connectedCallback() {
+        const htmlFile = '/pages/pag-navbar/footer.html'; // Replace with the actual path to your HTML file
+        try {
+            const response = await fetch(htmlFile);
+            if (!response.ok) throw new Error('Network response was not ok');
+
+            const content = await response.text();
+            console.log('Fetched content:', content);
+            this.innerHTML = content;
+        } catch (error) {
+            console.error('Failed to load the HTML file:', error);
+            this.innerHTML = '<p>Error loading content</p>';
+        }
+    }
+}
+
+customElements.define('special-footer', SpecialFooter)**/
