8d1ccc4ace6b6ddf29ba03f3ad96a0e595497b96
Jessica Martinez <jessica.martinezalv2905@gmail.com>
2025-10-06 17:09:25 -0600

    feat: se agrega el loader por primera vez

--- js/components.js
+++ js/components.js
@@ -36,3 +36,42 @@ customElements.define('special-header', SpecialHeader);
 }
 
 customElements.define('special-footer', SpecialFooter)**/
+
+/**
+ * Sección para el LOADER
+ */
+class SpecialLoader extends HTMLElement {
+    async connectedCallback() {
+        // Bloquear scroll en o que esta el loader
+        document.body.style.overflow = 'hidden';
+        document.documentElement.style.overflow = 'hidden';
+        
+        const htmlFile = '/pages/pag-loader/loader.html';
+        try {
+            const response = await fetch(htmlFile);
+            if (!response.ok) throw new Error('Network response was not ok');
+            const content = await response.text();
+            this.innerHTML = content;
+            
+            window.addEventListener('load', () => {
+                this.classList.add('hidden');
+                
+                setTimeout(() => {
+                    // Restaurar scroll
+                    document.body.style.overflow = '';
+                    document.documentElement.style.overflow = '';
+                    this.remove();
+                }, 300);
+            });
+            
+        } catch (error) {
+            console.error('Failed to load the loader:', error);
+            // Restaurar scroll si falla la pagina y el usuario no se quede sin poder hacer scroll
+            document.body.style.overflow = '';
+            document.documentElement.style.overflow = '';
+            this.remove();
+        }
+    }
+}
+customElements.define('special-loader', SpecialLoader);
+
