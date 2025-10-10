6952389e4d01425ea18cd6f56ef8ddafe23f9333
Victor Martinez <vicmanuelg19@gmail.com>
2025-10-10 00:15:14 -0600

    feat:se agrega la interacción para agregar productos mostrando el total en precio de cada producto dependiendo la cantidad de producto seleccionada

--- js/cotizador.js
+++ js/cotizador.js
@@ -1,4 +1,4 @@
-
+//Sección para carrusel con swiper
 let swiper1 = new Swiper('.swiper-container-pastes', {
 	navigation: {
 	  nextEl: '#contenedor-collapse-productos-pastes .swiper-button-next',
@@ -108,3 +108,46 @@ let swiper4 = new Swiper('#contenedor-collapse-productos-regalar .swiper-contain
 	  },
 	} 
     });
+
+//----------------------- interacción para sumar o restar cantidad en en productos-------------------------
+const productos = document.querySelectorAll('#producto');
+console.log(productos);
+
+productos.forEach(producto => {
+  // Obtenemos los elementos específicos DE CADA producto
+  const btnRestar = producto.querySelector('#btn-restar');
+  const btnSumar = producto.querySelector('#btn-sumar');
+  const cantidadElemento = producto.querySelector('#cantidad');
+  const totalElemento = producto.querySelector('#total-producto');
+
+//Tomamos el precio del producto convertido a float
+  const precioBase = parseFloat(totalElemento.dataset.precio);
+
+  //Asignamos en 0 la cantidad para cada uno de los productos
+  let cantidad = 0;
+
+  // Función para actualizar la UI de este producto específico
+  function actualizarProducto() {
+    const total = cantidad * precioBase;
+    cantidadElemento.textContent = cantidad;
+    totalElemento.textContent = `Total: $${total}`;
+  }
+
+  // Asignamos el evento al botón de sumar
+  btnSumar.addEventListener('click', () => {
+    cantidad++;
+    actualizarProducto();
+  });
+
+  // Asignamos el evento al botón de restar
+  btnRestar.addEventListener('click', () => {
+    // Solo restamos si la cantidad es mayor a 0
+    if (cantidad > 0) {
+      cantidad--;
+      actualizarProducto();
+    }
+  });
+
+});
+
+//
