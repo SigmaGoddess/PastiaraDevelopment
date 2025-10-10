a45990e75d4ca944e3498c882a52e4e478032256
Victor Martinez <vicmanuelg19@gmail.com>
2025-10-09 20:11:54 -0600

    feat:Se agregan images de productos junto con titulo, se agrega caja de comentarios, se cambia color de border de sección de formulario

--- js/cotizador.js
+++ js/cotizador.js
@@ -1,5 +1,5 @@
 
-var swiper1 = new Swiper('.swiper-container-pastes', {
+let swiper1 = new Swiper('.swiper-container-pastes', {
 	navigation: {
 	  nextEl: '#contenedor-collapse-productos-pastes .swiper-button-next',
 	  prevEl: '#contenedor-collapse-productos-pastes .swiper-button-prev'
@@ -28,7 +28,7 @@ var swiper1 = new Swiper('.swiper-container-pastes', {
 	} 
     });
 
-var swiper2 = new Swiper('#contenedor-collapse-productos-volovanes .swiper-container', {
+let swiper2 = new Swiper('#contenedor-collapse-productos-volovanes .swiper-container', {
 	navigation: {
 	  nextEl: '#contenedor-collapse-productos-volovanes .swiper-button-next',
 	  prevEl: '#contenedor-collapse-productos-volovanes .swiper-button-prev'
@@ -57,17 +57,40 @@ var swiper2 = new Swiper('#contenedor-collapse-productos-volovanes .swiper-conta
 	} 
     });
 
-    var swiper2 = new Swiper('#contenedor-collapse-productos-panaderia .swiper-container', {
+let swiper3 = new Swiper('#contenedor-collapse-productos-panaderia .swiper-container-panaderia', {
+	
 	navigation: {
 	  nextEl: '#contenedor-collapse-productos-panaderia .swiper-button-next',
 	  prevEl: '#contenedor-collapse-productos-panaderia .swiper-button-prev'
 	},
 	slidesPerView: 0,
 	spaceBetween: 0,
+	breakpoints: {
+	  20: {
+		slidesPerView: 2,
+		spaceBetween: 30,
+	  
+	  },
+      815: {
+		slidesPerView: 3,
+		spaceBetween: 10,
+	  },
 
-    
+	  1010: {
+		slidesPerView: 4,
+		spaceBetween: 10,
+	  },
+	} 
+    });
 
-  
+let swiper4 = new Swiper('#contenedor-collapse-productos-regalar .swiper-container', {
+	
+	navigation: {
+	  nextEl: '#contenedor-collapse-productos-regalar .swiper-button-next',
+	  prevEl: '#contenedor-collapse-productos-regalar .swiper-button-prev'
+	},
+	slidesPerView: 0,
+	spaceBetween: 0,
 	breakpoints: {
 	  20: {
 		slidesPerView: 2,
