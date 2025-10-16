b1052e4cdf46445a726ad1ff7bd2229ca625f9f7
Sheila Miranda <shei_xdxdxd@hotmail.com>
2025-10-16 11:08:45 -0600

    se agregan pag a vite config

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,8 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 conocenos: resolve(__dirname, 'pages/pag-conocenos/conocenos.html'),
-                menu: resolve(__dirname, 'pages/pag-navbar/navbar.html'),
-                /*aboutUs: resolve(__dirname, 'src/pages/about-us/about-us.html'),
-                contact: resolve(__dirname, 'src/pages/contact/contact.html'),*/
+                navbar: resolve(__dirname, 'pages/pag-navbar/navbar.html'),
+                footer: resolve(__dirname, 'pages/pag-footer/footer.html'),
                 registro: resolve(__dirname, 'pages/pag-registro/registro.html'),
                 perfilDeUsuario: resolve(__dirname, 'pages/pag-perfilDeUsuario/perfil_usuario.html'),
                 regalar: resolve(__dirname, 'pages/pag-productosParaRegalar/pag-regalar.html'),
