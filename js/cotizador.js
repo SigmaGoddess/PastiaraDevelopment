d9e626efc7568ada76b04efc18baa20117b832ae
Sheila Miranda <shei_xdxdxd@hotmail.com>
2025-10-31 15:06:30 -0600

    ..

--- js/cotizador.js
+++ js/cotizador.js
@@ -183,6 +183,7 @@ const entradaCodigoPostal = document.getElementById('entradaCodigoPostal');
 const entradaEstado = document.getElementById('entradaEstado');
 const entradaProductos = document.querySelectorAll('#cantidad')
 const alerta = document.getElementById('alerta');
+
 // --- CÓDIGO MODIFICADO ---
 // Variable para guardar los datos de la cotización que se mostrará en el modal.
 // La declaramos aquí para que sea accesible desde la función de confirmar y enviar.
@@ -408,7 +409,5 @@ document.addEventListener('DOMContentLoaded', () => {
   if (!token) {
     // Opción A: redirigir inmediatamente
     window.location.href = '/pages/pag-registro/registro.html#login-form';
-
   }
 })
-
